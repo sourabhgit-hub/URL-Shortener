2f8e043d58b49db78426ade737660618c7c445db
Sourabh <99796775+sourabhgit-hub@users.noreply.github.com>
2023-08-20 13:25:42 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pyshorteners as ps
 
-
-
 def index(request):
-    shorten_url='NA'
+    shorten_url=''
     try:
         if request.method=="POST":
             url=request.POST.get("website-link")
             s = ps.Shortener()
             shorten_url=s.tinyurl.short(url)
     except:
-        return HttpResponse("Some Error Occured");
+        return render(request, 'index.html',{"link":shorten_url})
     return render(request, 'index.html',{"link":shorten_url})
